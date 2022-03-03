e2d625a988954d35d149c1b01975bbf4e492fdf5
RohanK22 <rohank2205@gmail.com>
2022-03-03 19:21:14 +0000

    Use the fullfile function to get absolute paths to contour files

--- ARTwarp_Load_Data.m
+++ ARTwarp_Load_Data.m
@@ -20,7 +20,7 @@ for c1 = 1:numSamples
     clear tempres
     clear ctrlength
     clear fcontour
-    eval(['load ' append(DATA(c1).folder, '/', DATA(c1).name) ' -mat']);
+    eval(['load ' fullfile(DATA(c1).folder, DATA(c1).name) ' -mat']);
     if exist('ctrlength', 'var')
         DATA(c1).ctrlength = ctrlength;
         DATA(c1).length = length(freqContour);
