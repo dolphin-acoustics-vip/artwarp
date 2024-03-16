c157a6f07bef2846483deb118f7994cf5ed4e46a
Tate Dunbar <tatedunbar@8afb8410.st-andrews.ac.uk>
2024-03-16 16:01:20 +0000

    Add changes to Network.m

--- Network.m
+++ Network.m
@@ -5,7 +5,9 @@ classdef Network
     
     properties
         weights
-        
+        categories % Categories within the network
+        num_categories % Number of categories in the network
+        reclassifications % Number of reclassifications which have occurred
     end
     
     methods
@@ -13,13 +15,66 @@ classdef Network
             obj.weights = weights;
         end
 
-        function add(contour)
+        function obj = add(obj, contour)
             % Add a contour object to the network
             %   Iterates through the current categories and adds the
             %   contour to the one it is most similar to; if it matches
             %   none of them, create a new category for the contour
+            
+            matched_category = find_category(obj, contour);
 
-            % TODO
+            % If there is no category the contour fits into, create a new
+            % one, with the contour as the reference for it.
+            if matched_category == -1
+                new_category = Category.category(1, contour);
+                obj = obj.add_category(new_category);
+
+                % If the contour was already in a category, remove it
+                if(contour.category) ~= 0
+                    obj.categories(contour.category) = obj.categories(contour.category).remove(contour);
+                end
+                
+                % Increment number of reclassifications
+                obj.reclassifications = obj.reclassifications + 1;
+            elseif matched_category ~= contour.category
+                % Remove from old category
+                obj.categories(contour.category) = obj.categories(contour.category).remove(contour);
+
+                % Add to new category
+                obj.categories(matched_category) = obj.categories(matched_category).add(contour);
+
+                % Increment number of reclassifications
+                obj.reclassifications = obj.reclassifications + 1;
+            end
+            
+        end
+
+        function matched_category = find_category(obj, contour)
+            % Find the category the new contour fits into
+
+            % If there are no categories, a new one must be created
+            if obj.num_categories == 0
+                matched_category = -1;
+            else
+                % Create array for storing matches
+                a = zeros(1, obj.num_categories);
+                for i = 1:obj.num_categories
+                    a(i) = obj.categories(i).compare(contour);
+                end
+
+                % Find max match percentage and see if it is above
+                % vigilance, return 0 if not
+                [max_match, matched_category] = max(a);
+
+                if max_match < Parameters.vigilance
+                    matched_category = -1;
+                end
+            end
+        end
+
+        function obj = add_category(obj,cat)
+            obj.categories = [obj.categories, cat];
+            obj.num_categories = obj.num_categories + 1;
         end
     end
 end
